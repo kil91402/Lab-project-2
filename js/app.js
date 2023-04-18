2926cf1c84e5bfe5108602aaba61e2fefaba1074
Ajamu Akil Page <kil91402@gmail.com>
2023-04-18 00:25:55 +0000

    # 7 was changed and #45 through 53 was added

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let answerOne = prompt("Do I like to travel?").toLowerCase()
 let answerTwo = prompt("Am I a people person?").toLowerCase()
 let answerThree = prompt ("Do I love my job?").toLowerCase()
 let answerFour = prompt ("Did I win a state championship in High School?").toLowerCase()
-
+let answerFive = prompt ("Have I ever lived outside of the United States?").toLowerCase()
 
 // if 'y' then you are correct
 if(answerOne === 'y' ||
@@ -42,4 +42,12 @@ if(answerFour === 'y' ||
     // if 'n' then you are incorrect
     alert("You are incorrect!")
     console.log("You are incorrect!");
+} if(answerFive === 'n' ||
+answerFive ==='no'){
+   alert("You are correct!")
+   console.log("You are correct.");
+} else {
+   // if 'y' then you are incorrect
+   alert("You are incorrect!")
+   console.log("You are incorrect!");
 }
