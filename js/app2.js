0ecfb30893c91920a1f7aa367759e1c1098b2e54
Ajamu Akil Page <kil91402@gmail.com>
2023-04-20 21:38:18 -0500

    Create app2.js

---
+++ js/app2.js
@@ -0,0 +1,48 @@
+let number = prompt("Guess my favorite number")
+console.log("user guess num it: " + number)
+
+let favNum = 25;
+let counter = 0;
+
+while(counter <= 4){
+   
+    if (number < 25) {
+        alert('Sorry, too low!!');
+        counter++;
+    }
+    else if (number > 25) {
+        alert('Nope, too high!!');
+        counter++;
+    }
+    else if (number == 25) {
+        alert('Bust a move, you got it!!!');
+        break;
+    }
+
+    alert('Sorry, game over!! My favorite number is ' + favNum);
+}
+
+let answer;
+let correctAns = "football" || "basketball" || "tennis" || "baseball" || "golf" || "hockey";
+let count = 0
+let question = "Choose one of my favorite sports.";
+let a = true;
+
+while(a && count <=6){
+    answer = prompt (question);
+    if(answer === correctAns) {
+        console.log("Do your happy dance!!!");
+      a = false
+    
+    } else {
+        count++;
+        console.log("Sorry, Gameover.");
+    }
+      
+} 
+
+alert( 'Thanks for playing, goodbye.')
+
+//Lab-03 Done
+
+
